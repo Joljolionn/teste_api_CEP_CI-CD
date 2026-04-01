b78891e150628c19e73549824fbc291253584f05
Joljolionn <joljolionn@gmail.com>
2026-04-01 11:45:20 -0300

    feat: implementar testes

--- src/main/java/com/fatec/SS/WebController.java
+++ src/main/java/com/fatec/SS/WebController.java
@@ -1,76 +1,61 @@
 package com.fatec.SS;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WebController {
 
-  private final RestClient restClient;
+    private final CepService cepService;
 
-  public WebController() {
-    this.restClient = RestClient.create("https://viacep.com.br/ws/");
-  }
-
-  @GetMapping(path = "/{cep}")
-  public ResponseEntity<EnderecoDTO> returnEndereco(@PathVariable String cep) {
-    try {
-
-      EnderecoDTO endereco = restClient.get().uri("{cep}/json", cep).retrieve().body(EnderecoDTO.class);
-      return ResponseEntity.ok(endereco);
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(null);
+    public WebController(CepService cepService) {
+        this.cepService = cepService;
     }
-  }
 
-  @GetMapping(path = "/{cep}/logradouro")
-  public ResponseEntity<String> returnLogradouro(@PathVariable String cep) {
-
-    try {
-
-      EnderecoDTO endereco = restClient.get().uri("{cep}/json", cep).retrieve().body(EnderecoDTO.class);
-      return ResponseEntity.ok(endereco.logradouro());
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(null);
+    @GetMapping("/{cep}")
+    public ResponseEntity<EnderecoDTO> getEndereco(@PathVariable String cep) {
+        try {
+            return ResponseEntity.ok(cepService.buscarEndereco(cep));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
-  }
 
-  @GetMapping(path = "/{cep}/bairro")
-  public ResponseEntity<String> returnBairro(@PathVariable String cep) {
-
-    try {
-
-      EnderecoDTO endereco = restClient.get().uri("{cep}/json", cep).retrieve().body(EnderecoDTO.class);
-      return ResponseEntity.ok(endereco.bairro());
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(null);
+    @GetMapping("/{cep}/logradouro")
+    public ResponseEntity<String> getLogradouro(@PathVariable String cep) {
+        try {
+            return ResponseEntity.ok(cepService.buscarEndereco(cep).logradouro());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
-  }
 
-  @GetMapping(path = "/{cep}/localidade")
-  public ResponseEntity<String> returnLocalidade(@PathVariable String cep) {
-
-    try {
-
-      EnderecoDTO endereco = restClient.get().uri("{cep}/json", cep).retrieve().body(EnderecoDTO.class);
-      return ResponseEntity.ok(endereco.localidade());
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(null);
+    @GetMapping("/{cep}/bairro")
+    public ResponseEntity<String> getBairro(@PathVariable String cep) {
+        try {
+            return ResponseEntity.ok(cepService.buscarEndereco(cep).bairro());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
-  }
 
-  @GetMapping(path = "/{cep}/uf")
-  public ResponseEntity<String> returnUf(@PathVariable String cep) {
-
-    try {
-
-      EnderecoDTO endereco = restClient.get().uri("{cep}/json", cep).retrieve().body(EnderecoDTO.class);
-      return ResponseEntity.ok(endereco.uf());
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body(null);
+    @GetMapping("/{cep}/localidade")
+    public ResponseEntity<String> getLocalidade(@PathVariable String cep) {
+        try {
+            return ResponseEntity.ok(cepService.buscarEndereco(cep).localidade());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
-  }
+
+    @GetMapping("/{cep}/uf")
+    public ResponseEntity<String> getUf(@PathVariable String cep) {
+        try {
+            return ResponseEntity.ok(cepService.buscarEndereco(cep).uf());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
 }
